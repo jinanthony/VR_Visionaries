0fa438c039f83ced2c1669f9f1c3b93fee06bab6
Arul Saxena <saxena@ucsb.edu>
2024-05-27 14:14:40 -0700

    remove hardcoded vocab, add wordProvider

--- Assets/VRVisionProject/Button.cs
+++ Assets/VRVisionProject/Button.cs
@@ -8,14 +8,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Button : MonoBehaviour
 {
     public string stringInput = "";
-    public string[] wordBank = { "piano", "tiger", "horse", "sushi", "pineapple" };
-    public int wordBankIndex = 0;
+    //public string[] wordBank = { "piano", "tiger", "horse", "sushi", "pineapple" };
+    //public int wordBankIndex = 0;
 
     public TextMeshPro screenText;
 
     public XRBaseController leftController;
     public XRBaseController rightController;
-
 
     private void Start()
     {
@@ -38,7 +37,7 @@ public class Button : MonoBehaviour
         rightController.SendHapticImpulse(0.5f, 0.3f);
         leftController.SendHapticImpulse(0.5f, 0.3f);
 
-        if (stringInput == wordBank[0])
+        if (stringInput == WordProvider.getCurrentWord())
         {
            // Debug.Log("right SPELLING");
             screenText.color = Color.green;
---
+++ Assets/VRVisionProject/WordProvider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WordProvider
+{
+    public static string[] wordBank = { "piano", "tiger", "horse", "sushi", "pineapple" };
+    public static int wordBankIndex = 0;
+
+    public static string getCurrentWord()
+    {
+        return wordBank[wordBankIndex];
+    }
+}
